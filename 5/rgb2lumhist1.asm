832cbbe14ab4f4b51325108bb45c0334e2f54fca
kahlinbaughman <kahlin@hawaii.edu>
2019-03-04 19:27:58 -1000

    Finish exercise 2 question 1

--- 5/rgb2lumhist1.asm
+++ 5/rgb2lumhist1.asm
@@ -2,10 +2,17 @@
 
 segment .data
 	bin times 75 dd 0
+	lower dd 0
+	upper dd 9
+	count dd 0
+	luminance db "luminance ", 0
+	hyphen db "-"
+	colon db ": ", 0
+	
+
 segment .bss
 	width resd 1 ; Holds the width of the image
 	height resd 1 ; Holds the length of the image
- 	wcount resd 1
 
 segment .text
 	global asm_main
@@ -15,9 +22,11 @@ asm_main:
 	
 	call read_int ; Get the max width of the image
 	mov [width], eax ; Store the width
-	mov [wcount], eax
 	call read_int	; Get the max height of the image
 	mov [height], eax ; Store the height
+	
+	mov esi, [width] ; Move width into esi
+	mov edi, [height] ; Mov height into edi
 	
 height_loop:
 
@@ -32,7 +41,7 @@ width_loop:
 	add ebx, eax ; Add the next int into previous
 	
 	cmp ebx, 740
-	jbe bin_74
+	jae bin_74
 
 	mov eax, ebx ; Move r+g+b into eax
 	mov ecx, 10 ; Move 10 into ecx
@@ -40,15 +49,15 @@ width_loop:
 	div ecx ; Divide eax by ecx and store quotient into eax
 
 	mov ecx, bin ; move the address of bin into edx
-	mov edx, 0
-	mov ebx, 4
+	mov edx, 0 ; Set edx to 0 for multiplication
+	mov ebx, 4 ; Use four for int size
 	mul ebx ; multiply the quotient(index) by 4
 	add ecx, eax ; add the new index (eax) to the bin address
 	mov ebx, [ecx] ; Move whats at the new address into ebx
 	inc ebx ; Increment it by 1
-	mov [ecx], ebx ; mov the total back into edx
+	mov [ecx], ebx ; mov the total back into ecx
 	
-	;jmp loops
+	jmp loops
 bin_74:
 	mov ecx, bin ; move the address of bin into edx
 	mov edx, 0
@@ -58,26 +67,59 @@ bin_74:
 	add ecx, eax ; add the new index (eax) to the bin address
 	mov ebx, [ecx] ; Move whats at the new address into ebx
 	inc ebx ; Increment it by 1
-	mov [ecx], ebx ; mov the total back into edx
+	mov [ecx], ebx ; mov the total back into ecx
 
 loops:
-	mov eax, [wcount]
-	dec eax
-	mov [width], eax
-	cmp eax, 0
+	; Width Loop Logic Start (Inner loop)
+	dec esi
+	cmp esi, 0
 	jne width_loop
+	mov esi, [width]
+	; Width Loop Logic end (Inner Loop)
 	
-	;mov eax, [width]
-	;mov [wcount], eax
+	; Height Loop Logic Start (Outer)
+	dec edi
+	cmp edi, 0
+	jne height_loop
+	; Height Loop Logic end (Outer)
 
-	;mov eax, [height]
-	;dec eax
-	;mov [height], eax
-	;cmp eax, 0
-	;jne height_loop
-
-	dump_mem 2, ecx, 4
+print_loop:
 	
+	mov eax, luminance
+	call print_string
+	mov eax, [lower]
+	call print_int
+	mov eax, [hyphen]
+	call print_char
+	mov eax, [upper]
+	call print_int
+	mov eax, colon
+	call print_string
+	mov eax, [count]
+	mov edx, 0
+	mov ebx, 4
+	mul ebx
+	mov ebx, bin
+	add eax, ebx
+	mov eax, [eax]
+	call print_int
+	call print_nl
+	
+	mov eax, [lower]
+	add eax, 10
+	mov [lower], eax
+
+	mov eax, [upper]
+	add eax, 10
+	mov [upper], eax
+
+	mov eax, [count]
+	inc eax
+	mov [count], eax
+	cmp eax, 75
+	jne print_loop
+
+
 	popa
 	mov	eax, 0
 	leave
